7e6ef4a2f0c61281bdb090eeba1182ef5e781590
FeigMary <feigmary@gmail.com>
2023-06-03 15:30:44 +0300

    Task1 1

--- src/main/java/Sem4/Task1.java
+++ src/main/java/Sem4/Task1.java
@@ -1,4 +1,29 @@
 package Sem4;
 
+// Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+import java.util.LinkedList;
+
 public class Task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = createLinkedList(10);
+        System.out.println("Дан список элементов: " + list);
+        System.out.println("Перевернутый список: " + reverseList(list));
+    }
+
+    static LinkedList<Integer> createLinkedList(int size) {
+        LinkedList<Integer> LinkedList = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            LinkedList.add(i + 1);
+        }
+        return LinkedList;
+    }
+
+    static LinkedList<Integer> reverseList(LinkedList<Integer> LinkedList) {
+        LinkedList<Integer> newList = new LinkedList<>();
+        while (LinkedList.size() > 0) {
+            newList.add(LinkedList.pollLast());
+        }
+        return newList;
+    }
 }
